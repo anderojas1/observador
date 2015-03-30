2cbdbfa99fae03e843417dfda6c6ffaf9ac0c202
Anderson Enríquez <anderojas@anderojas-PC.home>
2015-03-30 16:50:59 -0500

    iomplementacion clase cuenta

--- src/Cuenta.java
+++ src/Cuenta.java
@@ -10,6 +10,42 @@
  */
 public class Cuenta {
     
+    double monto = 0.0;
+    String numeroCuenta = null;
+    Cliente persona = null;
+    
+    public Cuenta (double monto, String numero, Cliente cliente) {
+        
+        this.monto = monto;
+        numeroCuenta = numero;
+        persona = cliente;
+        
+    }
+
+    public double getMonto() {
+        return monto;
+    }
+
+    public void setMonto(double monto) {
+        this.monto = monto;
+    }
+
+    public String getNumeroCuenta() {
+        return numeroCuenta;
+    }
+
+    public void setNumeroCuenta(String numeroCuenta) {
+        this.numeroCuenta = numeroCuenta;
+    }
+
+    public Cliente getPersona() {
+        return persona;
+    }
+
+    public void setPersona(Cliente persona) {
+        this.persona = persona;
+    }
+    
     
     
 }
