129712a41fc9dc758aae26fe52a47daec98aa866
Juan Sebastian <juan.sebastian.rivas@correounivalle.edu.co>
2015-03-30 20:41:19 -0500

    Actualizacion

--- src/CorreoUsuario.java
+++ src/CorreoUsuario.java
@@ -9,16 +9,64 @@
  * @author android
  */
 
-import javax.swing.*;
+import java.awt.*;
 import java.util.*;
+import javax.swing.*;
 import java.awt.event.*;
 
 public class CorreoUsuario extends JFrame implements Observer, ActionListener {
 
+    private JPanel panelPrincipal;
+    private JLabel labelTitulo, labelMensaje;
+    private JButton buttonSalir;
+    private Cajero objCajero;
+    
+    public CorreoUsuario (Cajero cajeroObservado) 
+    {
+        objCajero = cajeroObservado;
+        
+        panelPrincipal = new JPanel();
+        
+        labelTitulo = new JLabel();
+        labelTitulo.setText("Correo Electronico");
+        labelTitulo.setBounds(20, 10, 70, 40);
+        
+        labelMensaje = new JLabel();
+        labelMensaje.setText("");
+        labelMensaje.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        labelMensaje.setBounds(20, 50, 300, 100);
+        
+        buttonSalir = new JButton();
+        buttonSalir.setText("Salir");
+        buttonSalir.setBounds(20, 160, 80, 20);
+        buttonSalir.addActionListener(this);
+        
+        add(labelTitulo);
+        add(labelMensaje);
+        add(buttonSalir);
+        
+        setSize(380, 200);
+        setTitle("Computador -> Correo Electronico");
+        setLocation(280, 300);
+        setLayout(null);
+        setVisible(true);
+        
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+    
     @Override
-    public void update(Observable o, Object arg) {}
+    public void update(Observable o, Object arg) 
+    {
+        labelMensaje.setText(objCajero.getMensaje());
+    }
 
     @Override
-    public void actionPerformed(ActionEvent e) {}
+    public void actionPerformed(ActionEvent e) 
+    {
+        if (e.getSource() == buttonSalir)
+        {
+            System.exit(0);
+        }
+    }
     
 }
--- src/main.java
+++ src/main.java
@@ -13,6 +13,11 @@ public class main
     public static void main (String [] args) 
     {
         Cajero objCajero = new Cajero();
+        MovilUsuario movil = new MovilUsuario(objCajero);
+        CorreoUsuario correo = new CorreoUsuario(objCajero);
+        
+        objCajero.addObserver(movil);
+        objCajero.addObserver(correo);
         objCajero.activarCajero();
     }
 }
