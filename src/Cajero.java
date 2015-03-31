6d60d7349ce36d08b6d809c6f7ec2e5e542b9098
Juan Sebastian <juan.sebastian.rivas@correounivalle.edu.co>
2015-03-30 19:51:40 -0500

    Actualizacion Clase Cajero, Creacion Clase Main

--- src/Cajero.java
+++ src/Cajero.java
@@ -10,53 +10,78 @@
  */
 
 import javax.swing.*;
+import java.util.*;
 
-public class Cajero {
+public class Cajero extends Observable{
     
-    static Banco banco = new Banco();
+    private String mensaje = "";
+    private Date fechaHora = new Date ();
+    private Banco banco = new Banco();
  
-    public static void main (String [] args) {
-        
+    public void setMensaje(String mensaje)
+    {
+        this.mensaje = mensaje;
+        setChanged();
+        notifyObservers();
+    }
+    
+    public String getMensaje()
+    {
+        return mensaje;
+    }
+    
+    public void activarCajero () 
+    {
         int opt = 0;
         int codop = 0;
+        String msg;
         
         do {
-
             try {
-
                 opt = Integer.parseInt(JOptionPane.showInputDialog("0 - Consignar\n1 - Retirar\n2 - Salir"));
                 
                 switch (opt) {
-
                     case 0: {
                         
                         String numeroCuenta = JOptionPane.showInputDialog("Ingrese el número de cuenta");
                         double monto = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el monto"));
                         banco.consignarMonto(numeroCuenta, monto);
                         
+                        msg = "Transaccion Exitosa\n Consigancion en la cuenta " 
+                                + numeroCuenta + " por el valor de " + monto + "\n Fecha y hora de la transaccion " 
+                                + fechaHora.toString();
+                        
+                        setMensaje(msg);
+                        
                     }break;
+                        
                     case 1: {
                         
                         String numeroCuenta = JOptionPane.showInputDialog("Ingrese el número de cuenta");
                         double monto = Double.parseDouble(JOptionPane.showInputDialog("Ingrese el monto"));
+                        
                         codop = banco.retirarMonto(numeroCuenta, monto);
-                        if (codop == -1) System.out.println("saldo insuficiente");
+                        
+                        if (codop == -1) JOptionPane.showMessageDialog(null, "saldo insuficiente");
+                        else
+                        {
+                            msg = "Transaccion Exitosa\n Retiro en la cuenta " 
+                                + numeroCuenta + " por el valor de " + monto + "\n Fecha y hora de la transaccion " 
+                                + fechaHora.toString();
+                            
+                            setMensaje(msg);
+                        }
                         
                     } break;
                         
-                    case 2: System.err.println("Hasta luego");break;
+                    case 2: JOptionPane.showMessageDialog(null, "Hasta luego");break;
                         
                     default: JOptionPane.showMessageDialog(null, "Ingrese una opción valida"); break;
-                        
-                }                
-
+                }
             } catch (Exception ex) {
-
                 System.err.println(ex.getMessage());
-                
             }
-
+            
         } while (opt != 2);
-        
     }
 }
